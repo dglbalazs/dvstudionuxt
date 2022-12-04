f76e4b2625856a65e3d12eb3c6b7abbf4871b7c8
diagolbalazs <diagolbalazs@gmail.com>
2022-12-04 11:56:51 +0100

    nuxt config js modification

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,10 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  targert: "static",
+  ssr: false,
+  generate :{ 
+    fallback : true
+  },
   head: {
     title: 'dvstudionuxt',
     htmlAttrs: {
